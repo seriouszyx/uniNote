52b90157b6dc82ce96d767a60cf94899736c01aa
seriouszyx <674965440@qq.com>
2018-07-15 18:37:17 +0800

    工程基本部署完成

---
+++ src/dao/dao/impl/UserDaoImpl.java
@@ -0,0 +1,11 @@
+package dao.dao.impl;
+
+/**
+ * @ClassName UserDaoImpl
+ * @Description TODO
+ * @Author Yixiang Zhao
+ * @Date 2018/7/15 18:19
+ * @Version 1.0
+ */
+public class UserDaoImpl {
+}
