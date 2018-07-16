0553c5b807ffa2978a0a861f0b05b09148cae66c
seriouszyx <674965440@qq.com>
2018-07-16 11:57:17 +0800

    新建用于注册的servlet,dao层,service层,已完成用户注册的大部分功能

--- src/service/service/impl/UserServiceImpl.java
+++ src/service/service/impl/UserServiceImpl.java
@@ -1,11 +1,23 @@
 package service.service.impl;
 
+import dao.UserDao;
+import dao.dao.impl.UserDaoImpl;
+import domain.User;
+import service.UserService;
+
+import java.sql.SQLException;
+
 /**
  * @ClassName UserServiceImpl
- * @Description TODO
+ * @Description 处理User的相关操作
  * @Author Yixiang Zhao
  * @Date 2018/7/15 18:21
  * @Version 1.0
  */
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
+    @Override
+    public void userSignup(User user) throws SQLException {
+        UserDao dao = new UserDaoImpl();
+        dao.userSignup(user);
+    }
 }
