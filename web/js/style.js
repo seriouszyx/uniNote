55a247d067b53f50eff6a161f4d789c2b88da62a
StathamWYJ <903875165@qq.com>
2018-07-16 17:38:24 +0800

    主页面，用户信息，笔记本

---
+++ web/js/style.js
@@ -0,0 +1,19 @@
+$(function() {
+				$('#order_nav li').hover(function() {
+						$(this).addClass('on');
+					},
+					function() {
+						$(this).removeClass('on');
+					}
+				);
+			});
+			$(function(){
+	// 初始化 传入dom id
+	var victor = new Victor("container", "output");
+	var theme = [
+			["#002c4a", "#005584"],
+			["#35ac03", "#3f4303"],
+			["#ac0908", "#cd5726"],
+			["#18bbff", "#00486b"]
+		]
+});
