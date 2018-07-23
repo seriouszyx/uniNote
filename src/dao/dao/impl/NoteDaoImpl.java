693086c7b5a5079cc2ea268ad43153b0eb0e4064
seriouszyx <674965440@qq.com>
2018-07-23 15:54:59 +0800

    调用编辑器API

---
+++ src/dao/dao/impl/NoteDaoImpl.java
@@ -0,0 +1,11 @@
+package dao.dao.impl;
+
+/**
+ * @ClassName NoteDaoImpl
+ * @Description TODO
+ * @Author Yixiang Zhao
+ * @Date 2018/7/23 15:29
+ * @Version 1.0
+ */
+public class NoteDaoImpl {
+}
---
+++ src/service/service/impl/NoteServiceImpl.java
@@ -0,0 +1,11 @@
+package service.service.impl;
+
+/**
+ * @ClassName NoteServiceImpl
+ * @Description TODO
+ * @Author Yixiang Zhao
+ * @Date 2018/7/23 15:28
+ * @Version 1.0
+ */
+public class NoteServiceImpl {
+}
